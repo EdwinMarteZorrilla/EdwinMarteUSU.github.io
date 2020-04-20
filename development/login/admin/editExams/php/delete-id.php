0abc522f6d263df4c4419f1a7f37982102595d92
Eduardo Cordova <ecordovap@hotmail.com>
2020-04-20 07:57:50 -0600

    Added deleted id link and question to journal.

--- development/login/admin/editExams/php/delete-id.php
+++ development/login/admin/editExams/php/delete-id.php
@@ -1,12 +1,14 @@
 <?php
 
 require_once('../../../../../include/config.php');
+require_once('/var/www/html/NHR-Core/development/login/admin/journal/journalFunctions.php');
 
 $connect = mysqli_connect("127.0.0.1:3306", "root", DB_PASS, "exams");
 $sql = "DELETE FROM ids" . $_POST['exam'] . " WHERE study_id='" . $_POST['id'] . "'";
 
 $result = mysqli_query($connect,$sql);
 if($result){
+  addEntryEvent(date("Y/m/d"), $_POST['exam'], "Deleted IDs from exam", "");
   echo '{"result": true}';
 }
 else{
--- development/login/admin/editExams/php/delete-question.php
+++ development/login/admin/editExams/php/delete-question.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once('../../../../../include/config.php');
+require_once('/var/www/html/NHR-Core/development/login/admin/journal/journalFunctions.php');
 
 $connect = mysqli_connect("127.0.0.1:3306", "root", DB_PASS, "exams");
 $sql = "DELETE FROM " . $_POST['exam'] . " WHERE question_id='" . $_POST['id'] . "'";
@@ -8,6 +9,7 @@ $sql2 = "DELETE FROM answers" . $_POST['exam'] . " WHERE question_id='" . $_POST
 $result = mysqli_query($connect,$sql);
 $result2 = mysqli_query($connect,$sql2);
 if($result && $result2){
+  addEntryEvent(date("Y/m/d"), $_POST['exam'], "Deleted question from exam", "");
   echo '{"result": true}';
 }
 else{
